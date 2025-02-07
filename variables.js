6500f2995d7d426cb7bc4239d0ea27964fd33f9c
Daniel <danielturcanu15@gmail.com>
2025-02-07 11:33:00 +0200

    variables tasks

---
+++ variables.js
@@ -0,0 +1,21 @@
+// Task 1
+// Create a variable with the name of admin and the value of John.
+
+let admin;
+let name;
+name = "John";
+
+admin = name;
+alert(admin); // John;
+
+// Task 2
+// Create a variable with the name of our planet. How would you name such a variable?
+
+let ourPlanetname = "Earth";
+let currentVisitorName = "John";
+
+// Task 3
+// Create a variable age and set it to null.
+
+const BIRTHDAY = "18.04.1982";
+const age = someCode(BIRTHDAY);

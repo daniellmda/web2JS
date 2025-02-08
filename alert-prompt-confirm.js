9504f7a9b6185cf8de69198169f829e58e54d4f7
Daniel <danielturcanu15@gmail.com>
2025-02-08 14:23:11 +0200

    2.1 - 2.6

---
+++ alert-prompt-confirm.js
@@ -0,0 +1,13 @@
+// Task 1
+let age = prompt("How old are you?", 100);
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+// Task 2
+
+let isBoss = confirm("You are here the main boss?");
+
+alert(isBoss); // true, if OK is pressed
+
+// Task 3
+let name = prompt("What is your name?", "John");
+alert("Your name is " + name); // John (if OK is pressed without any input)
---
+++ hello-world.js
@@ -0,0 +1,4 @@
+// // Task 1
+
+ alert("I am Javascript"); // I am Javascript
+
